de91c99a1b2fae3b1418e615e89480056ab0aaa0
Stephane Demingongo Litemo <sdemingongo@gmail.com>
2021-04-04 00:46:59 +0200

    update

--- index.js
+++ index.js
@@ -1,50 +1,66 @@
-const Joi = require("@hapi/joi");
-const Extend = require("extend");
-const Log = require("@novice1/logger").debugger("@novice1:validator:joi");
+const Joi = require('joi');
+const Extend = require('extend');
+const Log = require('@novice1/logger').debugger('@novice1/validator-joi');
 
-const PARAMETERS_PROPS = ["params", "body", "files", "query"];
+const PARAMETERS_PROPS = ['params', 'body', 'files', 'query'];
 
 /**
  *
- * @param {object} [options] hapi/joi options. Default: { stripUnknown: true }
+ * @param {object} [options] joi options. Default: { stripUnknown: true }
  * @param {Function} [onerror]
+ * @param {string} [validationProperty]
  */
-function validatorJoi(options, onerror) {
+function validatorJoi(options, onerror, validationProperty) {
   options = options || { stripUnknown: true };
 
   return function validateRequest(req, res, next) {
-    var toValidate = {};
-    var schema;
+    let schema;
+    const toValidate = {};
 
+    // retrieve schema
     if (
       req.meta &&
       req.meta.parameters &&
-      typeof req.meta.parameters === "object"
+      typeof req.meta.parameters === 'object'
     ) {
       schema = req.meta.parameters;
+
+      if (validationProperty && typeof validationProperty === 'string') {
+        if (
+          schema[validationProperty] &&
+          typeof schema[validationProperty] === 'object'
+        ) {
+          schema = schema[validationProperty];
+        } else {
+          schema = null;
+        }
+      }
+    }
+
+    // check if schema is a valid schema
+    if (schema) {
       // if it is not a Joi object
-      if (typeof schema.validate !== "function") {
-        var tmpSchema = {};
-        PARAMETERS_PROPS.forEach(
-          p => {
-            if(schema[p] && typeof schema[p] === "object") {
-              tmpSchema[p] = schema[p];
-            }
+      if (typeof schema.validate !== 'function') {
+        const tmpSchema = {};
+        PARAMETERS_PROPS.forEach((p) => {
+          if (schema[p] && typeof schema[p] === 'object') {
+            tmpSchema[p] = schema[p];
           }
-        );
+        });
         schema = tmpSchema;
-        if(Object.keys(schema).length){
+        if (Object.keys(schema).length) {
           schema = Joi.object(schema);
         }
       }
       // if still not a Joi object
-      if (schema.type !== "object") {
+      if (schema.type !== 'object') {
         schema = null;
       }
     }
 
+    // no schema to validate
     if (!schema) {
-      Log.silly("no schema to validate");
+      Log.silly('no schema to validate');
       return next();
     }
 
@@ -54,23 +70,27 @@ function validatorJoi(options, onerror) {
       }
     });
 
-    Log.info('validating %O', toValidate)
+    Log.info('validating %O', toValidate);
 
+    // validate schema
     return schema.validateAsync(toValidate, options).then(
       (validated) => {
-        Log.info("Valid request for %s", req.originalUrl);
+        Log.info('Valid request for %s', req.originalUrl);
         Extend(req, validated);
         next();
       },
       (err) => {
-        Log.error("Invalid request for %s", req.originalUrl);
-        if(typeof req.meta.parameters.onerror === "function") {
-          Log.error("Custom function onerror => %s", req.meta.parameters.onerror.name);
+        Log.error('Invalid request for %s', req.originalUrl);
+        if (typeof req.meta.parameters.onerror === 'function') {
+          Log.error(
+            'Custom function onerror => %s',
+            req.meta.parameters.onerror.name
+          );
           return req.meta.parameters.onerror(err, req, res, next);
         }
         if (onerror) {
-          if (typeof onerror === "function") {
-            Log.error("Custom function onerror => %s", onerror.name);
+          if (typeof onerror === 'function') {
+            Log.error('Custom function onerror => %s', onerror.name);
             return onerror(err, req, res, next);
           } else {
             Log.warn(
