2c1abcf1ba05e072b100f0fd90e154b78b2e7250
demingongo <sdemingongo@gmail.com>
2025-04-24 11:55:34 +0200

    types

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,8 +3,6 @@ export { };
 declare global {
   namespace Express {
     interface Request {
-      // add a property
-      files?: unknown;
 
       validated?<Q = Record<string, unknown>, P = unknown, B = unknown, H = unknown, C = unknown, F = unknown>(): {
         params: P | undefined
